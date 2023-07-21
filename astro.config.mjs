78fa33a4ddd0f8ae8a03234a9930c67480ab2edb
Andre Bellmann <andre.bellmann@huk-coburg.de>
2023-07-21 14:37:08 +0200

    start with layouting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import image from "@astrojs/image";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), image()]
+  integrations: [mdx(), sitemap(), image(), tailwind()]
 });
