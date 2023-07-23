9f1894e4e19cfe7a243b9de244c4372c250c9348
ingomc <janick2002@gmx.de>
2023-07-23 21:24:29 +0200

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {},
-  },
+    theme: {
+      extend: {
+        typography: {
+          DEFAULT: {
+            css: {
+              maxWidth: '120ch', // add required value here
+            }
+          }
+        }
+      },
+    },
   plugins: [require("@tailwindcss/typography")],
 };
