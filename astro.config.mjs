2064ae3cb1cc809cff43bb2c061e25e232e408db
ingomc <janick2002@gmx.de>
2023-07-23 22:42:01 +0200

    more styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
+import { astroImageTools } from "astro-imagetools";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), image(), tailwind()],
+  integrations: [mdx(), sitemap(), image(), tailwind(), astroImageTools],
 });
