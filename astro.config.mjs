5111128119f63fa715ce942569eebf10707edc54
ingomc <janick2002@gmx.de>
2023-07-28 21:23:57 +0200

    local netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,42 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import { astroImageTools } from "astro-imagetools";
+import NetlifyCMS from "astro-netlify-cms";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), image(), tailwind(), astroImageTools],
+  integrations: [
+    mdx(),
+    sitemap(),
+    image(),
+    tailwind(),
+    astroImageTools,
+    NetlifyCMS({
+      config: {
+        backend: {
+          squash_merges: true,
+          name: "git-gateway",
+          branch: "main",
+        },
+        locale: "de",
+        collections: [
+          {
+            name: "start",
+            label: "Startseite",
+            folder: "src/content/start",
+            create: true,
+            slug: "{{slug}}",
+            fields: [
+              { label: "Title", name: "title", widget: "string" },
+              { label: "Reihenfolge", name: "order", widget: "number" },
+              { label: "Body", name: "body", widget: "markdown" },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
 });
