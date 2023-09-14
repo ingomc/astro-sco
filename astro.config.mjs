8d121650264280a00c5dcd4ce191dfdedc6894b9
Andre Bellmann <andre.bellmann@huk-coburg.de>
2023-09-14 17:28:51 +0200

    start

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
 import { astroImageTools } from "astro-imagetools";
 
 import tailwind from "@astrojs/tailwind";
@@ -9,5 +8,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), image(), tailwind(), astroImageTools],
+  integrations: [mdx(), sitemap(), tailwind(), astroImageTools],
 });
