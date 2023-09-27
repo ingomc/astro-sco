57bc8a8a9bf1d1488d228f2d0e853d0e30d88adc
ingomc <janick2002@gmx.de>
2023-09-27 21:45:37 +0200

    favicon und titles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "My personal website.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "SCO-OGV Oberfüllbach 1963 e.V.";
+export const SITE_DESCRIPTION = "Der SCO-OGV Oberfüllbach 1963 e.V. ist ein offener und familiärer Verein, bei dem neben dem sportlichen Betätigung auch das gesellschaftliche Leben eine wichtige Rolle spielt.";
