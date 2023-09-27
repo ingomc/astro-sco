11c92aa3a1daa22e313be489ba1fcfb32996d94d
ingomc <janick2002@gmx.de>
2023-09-27 21:45:37 +0200

    favicon und titles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://www.sc-oberfuellbach.de/",
   integrations: [mdx(), sitemap(), tailwind(), astroImageTools],
 });
