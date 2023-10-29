cfe5842cb4d3e19be9f75fcc72b7b9d8d561f678
ingomc <janick2002@gmx.de>
2023-10-29 14:59:01 +0100

    add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,26 @@ import sitemap from "@astrojs/sitemap";
 import { astroImageTools } from "astro-imagetools";
 
 import tailwind from "@astrojs/tailwind";
+import dotenv from 'dotenv';
+
+const deployTarget = process.env.DEPLOY_TARGET
+
+console.log("Deploy Target: " + deployTarget);
+
+const deployadapter = () => {
+  if (deployTarget === 'netlify') {
+    return undefined;
+  }
+  return (vercel({ webAnalytics: {
+    enabled: true,
+  },}))
+}
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.sc-oberfuellbach.de/",
   integrations: [mdx(), sitemap(), tailwind(), astroImageTools],
   output: 'static',
-  adapter: vercel({ webAnalytics: {
-    enabled: true,
-  },}),
+  adapter: deployadapter(),
 });
