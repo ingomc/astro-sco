707a88b767f1ca869bf505840a46ba861a6a60ba
ingomc <janick2002@gmx.de>
2024-06-16 14:08:09 +0200

    remove astroimagetools completely

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/static";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { astroImageTools } from "astro-imagetools";
 
 import tailwind from "@astrojs/tailwind";
 import dotenv from "dotenv";
@@ -28,7 +27,7 @@ const deployadapter = () => {
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.sc-oberfuellbach.de/",
-  integrations: [mdx(), sitemap(), tailwind(), astroImageTools],
+  integrations: [mdx(), sitemap(), tailwind()],
   output: "static",
   adapter: deployadapter(),
 });
